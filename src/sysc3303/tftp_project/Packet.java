04ad0fe74f4c6b423db38d0fc8344380bec856ae
Korey Conway <korey.conway@gmail.com>
2013-05-15 14:49:07 -0400

    - Made Packet an abstract class - Packet stuff is still broken though

--- src/sysc3303/tftp_project/Packet.java
+++ src/sysc3303/tftp_project/Packet.java
@@ -1,26 +1,96 @@
 package sysc3303.tftp_project;
 
-public interface Packet {
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+
+public abstract class Packet {
+	protected Type type;
+	protected boolean isValid = true;
+
+	public enum Type {
+		RRQ, WRQ, DATA, ACK, ERROR
+	}
+
+	static void CreateReadRequestPacket() {
+		// TODO
+	}
+
+	static void CreateWriteRequestPacket() {
+		// TODO
+	}
+
+	static void CreateAckPacket() {
+		// TODO
+	}
+
+	static void CreateDataPacket() {
+		// TODO
+	}
+
+	static void CreateErrorPacket() {
+		// TODO
+	}
+
+	static Packet CreateFromBytes(byte[] data) throws InvalidPacketException {
+		// TODO
+		if (data[0] != 0) {
+			throw new InvalidPacketException();
+		}
+
+		switch (data[1]) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		default:
+			throw new InvalidPacketException();
+		}
+		return null;
+	}
 
 	/**
-	 * Validate the packet
+	 * @param destinationAddress
+	 * @param destinationPort
+	 * @return a DatagramPacket ready to be sent through a socket
+	 * @throws InvalidPacketException
+	 */
+	public DatagramPacket generateDatagram(InetAddress destinationAddress,
+			int destinationPort) throws InvalidPacketException {
+		byte data[] = this.generateData();
+		return new DatagramPacket(data, data.length, destinationAddress,
+				destinationPort);
+	}
+
+	/**
+	 * Check if the packet is valid
+	 * 
 	 * @return return true when the packet is valid, false otherwise
 	 */
-	public abstract boolean isValid();
+	public boolean isValid() {
+		return isValid;
+	}
 
 	/**
 	 * Generate the packet data
+	 * 
 	 * @return the byte array of the packet
 	 * @throws InvalidPacketException
 	 */
-	public abstract byte[] generatePacketData() throws InvalidPacketException;
+	public abstract byte[] generateData() throws InvalidPacketException;
 
 	/**
-	 * Convert the request into a visual packet string (for debugging/logging only)
+	 * Convert the request into a visual packet string (for debugging/logging
+	 * only)
+	 * 
 	 * @return a string representation of the packet
-	 * @throws InvalidPacketException;
+	 * @throws InvalidPacketException
 	 */
-	public abstract String generatePacketString() throws InvalidPacketException;
+	public abstract String generateString() throws InvalidPacketException;
 
 }
-
--- src/sysc3303/tftp_project/RequestPacket.java
+++ src/sysc3303/tftp_project/RequestPacket.java
@@ -3,100 +3,127 @@ package sysc3303.tftp_project;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-public class RequestPacket implements Packet {
-	public String filename = "";
-	public Action action;
-	public Mode mode;
-	protected boolean isValid = true;
-	public enum Action {READ, WRITE, INVALID}
-	public enum Mode {ASCII, OCTET}
-		
-	public RequestPacket(String filename, Action action, Mode mode)
-	{
+public class RequestPacket extends Packet {
+	protected String filename = "";
+	protected Action action;
+	protected Mode mode;
+	protected Mode defaultMode = Mode.OCTET;
+	
+	public enum Action {
+		READ, WRITE
+	}
+
+	public enum Mode {
+		ASCII, OCTET
+	}
+
+	/**
+	 * @param action
+	 * @param filename
+	 * @param mode
+	 * @throws IllegalArgumentException
+	 */
+	public RequestPacket(Action action, String filename) throws IllegalArgumentException {
+		if ( action == null || filename == null || filename.length() == 0 || mode == null ) {
+			throw new IllegalArgumentException();
+		}
 		this.filename = filename;
 		this.action = action;
-		this.mode = mode;
+		this.mode = defaultMode;
+		this.type = (action == Action.READ) ? Type.RRQ : Type.WRQ;
 	}
 	
-	public RequestPacket(byte[] packetData, int dataLength)
-	{
+	public static RequestPacket CreateReadRequest(String filename) {
+		return new RequestPacket(Action.READ, filename);
+	}
+
+	public static RequestPacket CreateWriteRequest(String filename) {
+		return new RequestPacket(Action.WRITE, filename);
+	}
+
+	public static RequestPacket CreateFromBytes(byte[] data, int dataLength) throws InvalidPacketException {
 		// Assume valid until we find an error
-		isValid = true;
-		
+		boolean isValid = true;
+
 		// Get the action (read/write)
-		if (packetData[0] == 0 && packetData[1] == 1) {
+		if (data[0] == 0 && data[1] == 1) {
 			action = RequestPacket.Action.READ;
-		} else if (packetData[0] == 0 && packetData[1] == 2) {
+		} else if (data[0] == 0 && data[1] == 2) {
 			action = RequestPacket.Action.WRITE;
 		} else {
 			isValid = false;
 		}
-		
+
 		// Extract the filename
 		int i = 1;
-		while (packetData[++i] != 0 && i < dataLength) {
-			filename += (char)packetData[i]; // Note: might be more efficient with StringBuilder
+		while (data[++i] != 0 && i < dataLength) {
+			filename += (char) data[i]; // Note: might be more efficient
+												// with StringBuilder
 		}
-					
+
 		// If this isn't 0 then we likely filled up the buffer (oops)
-		if (packetData[i] != 0) {
-			isValid = false;				
+		if (data[i] != 0) {
+			isValid = false;
 		}
-		
+
 		// Extract the transfer mode
 		String modeStr = "";
-		while (packetData[++i] != 0 && i < dataLength) {
-			modeStr += (char)packetData[i]; // Note: might be more efficient with StringBuilder
+		while (data[++i] != 0 && i < dataLength) {
+			modeStr += (char) data[i]; // Note: might be more efficient
+												// with StringBuilder
 		}
-		
+
 		modeStr = modeStr.toLowerCase();
-		if ( modeStr.equals("ascii") ) {
-			mode = Mode.ASCII; 
-		} else if ( modeStr.equals("octet") ) {
-			mode = Mode.OCTET; 
+		if (modeStr.equals("ascii")) {
+			mode = Mode.ASCII;
+		} else if (modeStr.equals("octet")) {
+			mode = Mode.OCTET;
 		}
-		
+
 		// Make sure there is just a 0 and no more data after
 		while (i < dataLength) {
-			if ( packetData[i++] != 0 ) {
+			if (data[i++] != 0) {
 				isValid = false;
 				break;
 			}
 		}
 	}
 
-	/*
-	 * Validate the request
-	 */
-	/* (non-Javadoc)
+	/**
+	 * Validate the packet
+	 * 
+	 * @return return true when the packet is valid, false otherwise
 	 * @see sysc3303.project.Packet#isValid()
 	 */
 	@Override
-	public boolean isValid()
-	{
-		if (!isValid) return false;
-		if (filename == null || filename.isEmpty()) return false;
-		if (mode == null) return false;
-		if (action == null || action == Action.INVALID) return false;
+	public boolean isValid() {
+		if (!isValid)
+			return false;
+		if (filename == null || filename.isEmpty())
+			return false;
+		if (mode == null)
+			return false;
+		if (action == null || action == Action.INVALID)
+			return false;
 		return true;
 	}
-	
-	/*
+
+	/**
 	 * Generate the packet data
-	 */
-	/* (non-Javadoc)
+	 * 
+	 * @return the byte array of the packet
+	 * @throws InvalidPacketException
 	 * @see sysc3303.project.Packet#generatePacketData()
 	 */
 	@Override
-	public byte[] generatePacketData()
-	{
+	public byte[] generateData() throws InvalidPacketException {
 		try {
 			// Form the byte array
 			ByteArrayOutputStream stream = new ByteArrayOutputStream();
-			
+
 			// Always start with 0
 			stream.write(0);
-			
+
 			// Set the request action type byte
 			switch (action) {
 			case READ:
@@ -111,50 +138,51 @@ public class RequestPacket implements Packet {
 			default:
 				throw new InvalidPacketException();
 			}
-			
+
 			// Add the filename
-			if ( null != filename ) {
+			if (null != filename) {
 				stream.write(filename.getBytes());
 			}
-			
+
 			// Add 0, as per the required format
 			stream.write(0);
-			
+
 			// Set the mode
 			stream.write(mode.toString().getBytes());
-			
+
 			// Finish with 0
 			stream.write(0);
-			
+
 			return stream.toByteArray();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (NullPointerException e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
-	/*
-	 * Convert the request into a visual packet string (for debugging/logging only)
-	 */
-	/* (non-Javadoc)
+
+	/**
+	 * Convert the request into a visual packet string (for debugging/logging
+	 * only)
+	 * 
+	 * @return a string representation of the packet
+	 * @throws InvalidPacketException
 	 * @see sysc3303.project.Packet#generatePacketString()
 	 */
 	@Override
-	public String generatePacketString()
-	{
+	public String generateString() throws InvalidPacketException {
 		try {
 			if (!isValid()) {
 				return "invalid packet";
 			}
-			
+
 			StringBuilder packetStr = new StringBuilder();
-			
+
 			// Always start with 0
 			packetStr.append(0);
-			
+
 			// Set the request action type byte
 			switch (action) {
 			case READ:
@@ -169,21 +197,21 @@ public class RequestPacket implements Packet {
 			default:
 				break;
 			}
-			
+
 			// Add the filename
 			if (null != filename) {
 				packetStr.append(filename);
 			}
-			
+
 			// Add 0, as per the required format
 			packetStr.append(0);
-			
+
 			// Set the mode
 			packetStr.append(mode.toString());
-			
+
 			// Finish with 0
 			packetStr.append(0);
-			
+
 			return packetStr.toString();
 		} catch (NullPointerException e) {
 			e.printStackTrace();
